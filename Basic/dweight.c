d8a2f8453f4b2abe9d2a85ce42232bc2e0336029
Jason Zhang <1515143570@qq.com>
2020-06-10 21:57:44 +0800

    add the dweight.c

---
+++ Basic/dweight.c
@@ -0,0 +1,30 @@
+/*
+ * Name: dweight.c
+ * Purpose: Computes the dimesional weight of a 12" x 10" x 8" box
+ * Author: Jason Zhang
+ */
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int height, length, width, volume, weight;
+	
+	printf("Enter height of box: ");
+	scanf("%d", &height);
+	
+	printf("Enter length of box: ");
+	scanf("%d", &length);
+
+	printf("Enter width of box: ");
+	scanf("%d", &width);
+
+	volume = height * length * width;
+	weight = (volume + 165) / 166;
+
+	printf("Dimesions: %dx%dx%d\n", length, width, height);
+	printf("Volume (cubic inches): %d\n", volume);
+	printf("Dimesional weight (pounds): %d\n", weight);
+
+	return 0;
+}
--- Basic/hello.c
+++ Basic/hello.c
@@ -1,3 +1,9 @@
+/*
+ * Name: hello.c
+ * Purpose: A simplest C program to print "Hello World" on the screen.
+ * Author: Jason Zhang
+ */
+
 #include <stdio.h>
 
 int main(int argc, char * argv[])
